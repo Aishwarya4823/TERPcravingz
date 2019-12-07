40b8a637cca918af1f53ab2092f2aaf1c23d4a59
Aishwarya Bhangale <aishwaryabhangale1997@gmail.com>
2019-12-06 20:35:06 -0500

    Changed 9 and 10 sql query number

--- Submitted Suite Documents/DML/0504_06_Query10.sql
+++ Submitted Suite Documents/DML/0504_06_Query10.sql
@@ -1,13 +1,12 @@
 --10.What are the names, addresses, phone numbers for restaurants that have high ranking AND are open on the weekend and has Review that includes: family OR friends?
 SELECT r.resId,r.resName,r.resStreet, r.resCity,r.resState,r.resPhoneNo, r.resStars, r.resPrice
-FROM Restaurant r,review e,write w, (
-	SELECT w.resId, r.resStars 
-	FROM Restaurant r, Write w, Review e
-	WHERE w.revId = e.revId
-	AND r.resId = w.resId
+FROM Restaurant r,review e, (
+	SELECT r.resId, r.resStars 
+	FROM Restaurant r, Review e
+	WHERE r.resId = e.resId
 	AND (e.revText Like '%family%'
 	OR e.revText Like '%friend%')) f
-WHERE r.resId=w.resId AND e.revId=w.revId
+WHERE r.resId = e.resId
 AND r.resId = f.resId 
 AND (r.satStartTime IS NOT NULL
 OR r.sunStartTime IS NOT NULL)
