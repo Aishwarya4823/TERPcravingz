3a8dc7473477db0b82bb220457d5dcf4f8af0b24
Aishwarya Bhangale <aishwaryabhangale1997@gmail.com>
2019-12-06 21:16:42 -0500

    Updated 9

--- Submitted Suite Documents/DML/0504_06_Query9.sql
+++ Submitted Suite Documents/DML/0504_06_Query9.sql
@@ -1,11 +1,7 @@
 --Need to remove write table  
 --9.What are the top 5 customers/foodies who have highest numbers of reviews on various restaurants?
-SELECT TOP 5 c.cusName, COUNT(r.revId) 
+SELECT TOP 5 c.cusName, COUNT(r.revId) AS 'the number of reviews'
 FROM Customer c, review r
 WHERE r.cusId = c.cusId
 GROUP BY c.cusId,c.cusName
-<<<<<<< HEAD
 ORDER BY COUNT(r.revId) DESC
-=======
-ORDER BY COUNT(w.revId) DESC
->>>>>>> 24f4c42c484db28e41e2b37d4635b531d5d96fbe
